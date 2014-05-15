720ff08334fa9944b9e8470d1f7c6478457ec8ec
Jake Davis <jake.davis5989@gmail.com>
2014-05-15 23:59:19 +0000

    [1.3.0] Eliminate default behavior for rabbitmq_config parameters

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer        'Simple Finance'
 maintainer_email  'ops@simple.com'
 license           'Apache 2.0'
 description       'Installs RabbitMQ, an Erlang message queue application'
-version           '1.2.0'
+version           '1.3.0'
 
 depends 'erlang'
