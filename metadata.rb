bc97f8ba6b2167d739652469e0e1074f3006cb35
Jake Davis <jake.davis5989@gmail.com>
2015-03-26 19:49:39 +0000

    Pin rabbitmq_http_api_client to 1.3.0

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer        'Simple Finance'
 maintainer_email  'ops@simple.com'
 license           'Apache 2.0'
 description       'Installs RabbitMQ, an Erlang message queue application'
-version           '1.5.1'
+version           '1.6.0'
 
 depends 'erlang'
